bc8a8c8d086b94adec83e3b92c0da2e1f9abefb0
SebeNn <sebasnio17@gmail.com>
2025-04-02 21:04:11 -0500

    Prueba ejecutable

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,6 @@ module com.formales.view {
     requires javafx.graphics;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.desktop;
 
     opens com.formales.view to javafx.fxml;
